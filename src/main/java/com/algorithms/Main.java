ca3f8375566cb0646683fce1bd3f8272a2da5fc9
Yeison Ramirez <yeisonrazar98@gmail.com>
2025-01-31 13:35:09 -0500

    Quick Sort Algorithm

--- src/main/java/com/algorithms/Main.java
+++ src/main/java/com/algorithms/Main.java
@@ -7,10 +7,12 @@ import com.algorithms.sort.SortAlgorithms;
 public class Main {
     public static void main(String[] args) {        
         int[] numbers = { 2, 4, 8, 0, 5, 1, 9, 3, 7, 6, 4, 2 };
+        //    numbers = { 0, 1, 2, 2, 5, 4, 9, 3, 7, 6, 4, 8 };
 
         // SortAlgorithms.bubbleSort(numbers);
         // SortAlgorithms.insertionSort(numbers);
-        SortAlgorithms.selectionSort(numbers);
+        // SortAlgorithms.selectionSort(numbers);
+        SortAlgorithms.quickSort(numbers, 0, numbers.length - 1);
 
         System.out.println(Arrays.toString(numbers));
     }
--- src/main/java/com/algorithms/sort/SortAlgorithms.java
+++ src/main/java/com/algorithms/sort/SortAlgorithms.java
@@ -56,8 +56,33 @@ public class SortAlgorithms {
         }
     }
 
-    public static void quickSort(int[] array, int low, int high) {
-        
+    public static void quickSort(int[] array, int start, int end) {
+        if( start < end ) {
+            int p = partition(array, start, end);
+            quickSort(array, start, p - 1);
+            quickSort(array, p + 1, end);
+        }     
+    }
+
+    private static int partition(int[] array, int start, int end) {
+        int pivot = array[ end ];
+
+        int pointer = start;
+
+        for (int j = start; j < end; j++) {
+            if (array[ j ] <= pivot) {
+                int temp = array[ j ];
+                array[ j ] = array[ pointer ];
+                array[ pointer ] = temp;
+                pointer++;
+            }
+        }
+
+        int temp = array[pointer];
+        array[ pointer ] = array[ end ];
+        array[ end ] = temp;
+
+        return pointer;
     }
 
 }
